cef77fd5c5f969784e63a9c6ffc543c22c6d50ca
Al-Farabi Fernandeze <abitpsycho6@gmail.com>
2025-02-26 10:01:34 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Hero from './components/hero';
+import Hero from './components/Hero';
 
 function App() {
   const [count, setCount] = useState(0)
