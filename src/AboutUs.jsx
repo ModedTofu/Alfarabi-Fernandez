cbb7249e13064767ca036667e526fc286c9d2bbd
Al-Farabi Fernandeze <abitpsycho6@gmail.com>
2025-03-09 10:07:14 +0800

    added about us

---
+++ src/AboutUs.jsx
@@ -0,0 +1,11 @@
+const AboutUs = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>Welcome to our website! We are passionate about building amazing applications.</p>
+      </div>
+    );
+  };
+  
+  export default AboutUs;
+  
