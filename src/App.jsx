2676aed2a502886dc45afa63451d3b9b2a15ab51
Al-Farabi Fernandeze <abitpsycho6@gmail.com>
2025-03-09 10:45:44 +0800

    casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Hero from "./components/Hero";
 import AboutUs from "./AboutUs";
-import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
