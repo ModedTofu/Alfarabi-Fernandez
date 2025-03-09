612141185b148aa48cccdf40de635803c12be0d8
Al-Farabi Fernandeze <abitpsycho6@gmail.com>
2025-03-09 10:21:53 +0800

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './components/Hero';
 import AboutUs from './AboutUs';
+import Navbar from './components/navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar/>
       <Hero/>
       <AboutUs />
     </>
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Create a new CSS file for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">My Portfolio</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
